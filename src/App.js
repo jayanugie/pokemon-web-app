7617ade1acaccc1a45db91b6dafa3efb3c913ff3
Nugie Jaya Nugraha <jayanugie14@gmail.com>
2022-10-17 23:47:21 +0700

    add navbar, input, axios get

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [pokemonName, setPokemonName] = useState("");
+
+  const searchPokemon = () => {
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+      .then((res) => {
+        console.log(res);
+      });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* navbar */}
+      <nav className="navbar bg-dark">
+        <div className="container">
+          <span className="navbar-brand mb-0 h1 text-white">Pokemon Stats</span>
+        </div>
+      </nav>
+
+      {/* content */}
+      <div className="">
+        <div className="container">
+          <div className="d-flex justify-content-center m-5">
+            <div className="input-group input-group-lg w-25">
+              <input
+                placeholder="Search Pokemon"
+                type="text"
+                className="form-control"
+                onChange={(e) => setPokemonName(e.target.value)}
+              />
+              <button
+                className="rounded-end border search"
+                onClick={searchPokemon}
+              >
+                <img src="/search.png" alt="search"/>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
